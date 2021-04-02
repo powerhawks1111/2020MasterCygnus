b091f179eb7ab6b42c5d2f6fd23172a1e73ce4bf
rex-mcall <rexmcall@gmail.com>
2021-04-02 17:55:18 -0400

    Added comments to the Scheduler

--- src/main/java/frc/robot/commands/Scheduler.java
+++ src/main/java/frc/robot/commands/Scheduler.java
@@ -7,16 +7,26 @@ public class Scheduler {
 
     boolean done = false;
     boolean running = false;
-    int i = 0;
+    int i = 0; //index of currently running command
     ArrayList<Command> commands;
 
-    public Scheduler() {
-    }
+    /**
+     * Default constructor (commands are added on later)
+     */
+    public Scheduler() {}
 
+    /**
+     * Overloaded constructor that accepts an existing ArrayList of commands
+     * @param coms
+     * <ul><li>ArrayList of commands</li></ul>
+     */
     public Scheduler(ArrayList<Command> coms) {
         commands = coms;
     }
 
+    /**
+     * Runs each command in the com ArrayList
+     */
     public void run() {
         Command com = commands.get(i);
         if (!done) {
@@ -35,10 +45,20 @@ public class Scheduler {
         }
     }
 
+    /**
+     * Accepts a list of commands
+     * @param coms
+     * <ul><li>ArrayList of commands</li></ul>
+     */
     public void addCommands(ArrayList<Command> coms) {
         commands = coms;
     }
 
+    /**
+     * Resets the scheduler to allow autonomous to run multiple times, if needed
+     * <br><br>
+     * Every variable is reset to it's original value
+     */
     public void reset() {
         try {
             commands.clear();
