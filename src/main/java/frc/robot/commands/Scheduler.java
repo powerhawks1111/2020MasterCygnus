05beb79aeae74da1ec6e4c6ef6cf766b58c7f5a8
rex-mcall <rexmcall@gmail.com>
2021-04-02 17:38:49 -0400

    Spaces > tabs

--- src/main/java/frc/robot/commands/Scheduler.java
+++ src/main/java/frc/robot/commands/Scheduler.java
@@ -5,50 +5,48 @@ import java.util.ArrayList;
 
 public class Scheduler {
 
-	boolean done = false;
-	boolean running = false;
-	int i = 0;
-	ArrayList<Command> commands;
+    boolean done = false;
+    boolean running = false;
+    int i = 0;
+    ArrayList<Command> commands;
 
-	public Scheduler() {
-		
-	}
-	
-	public Scheduler(ArrayList<Command> coms) {
-		commands = coms;
-	}
+    public Scheduler() {
+    }
 
-	public void run() {
-		Command com = commands.get(i);
-		if (!done) {
-			com.execute();
-		}
-		if (!com.isComplete()) {
-			running = true;
-		} else {
-			com.stop();
-			if (i < commands.size() - 1) {
-				i++;
-			} else {
-				done = true;
-				running = false;
-			}
-		}
-	}
+    public Scheduler(ArrayList<Command> coms) {
+        commands = coms;
+    }
 
-	public void addCommands(ArrayList<Command> coms) {
-		commands = coms;
-	}
+    public void run() {
+        Command com = commands.get(i);
+        if (!done) {
+            com.execute();
+        }
+        if (!com.isComplete()) {
+            running = true;
+        } else {
+            com.stop();
+            if (i < commands.size() - 1) {
+                i++;
+            } else {
+                done = true;
+                running = false;
+            }
+        }
+    }
 
-	public void reset() {
-		try {
-			commands.clear();
-			done = false;
-			running = false;
-			i = 0;
-		}
-		catch (Exception e) {
-			return;
-		}
-	}
+    public void addCommands(ArrayList<Command> coms) {
+        commands = coms;
+    }
+
+    public void reset() {
+        try {
+            commands.clear();
+            done = false;
+            running = false;
+            i = 0;
+        } catch (Exception e) {
+            return;
+        }
+    }
 }
